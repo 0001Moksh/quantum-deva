8f465549a178feb0143300514021f31ad8a9f724
MOKSH BHARDWAJ <164907693+0001Moksh@users.noreply.github.com>
2025-03-01 12:06:28 +0530

    allow all origin

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ allowed_origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=allowed_origins,
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET"],
     allow_headers=["*"],
 )
 
